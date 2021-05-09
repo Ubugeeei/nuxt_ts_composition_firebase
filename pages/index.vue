6077303eeda3c4209ec8802138cddef6e67e8d27
Ubugeeei <ubuge1122@gmail.com>
2021-05-09 20:27:21 +0900

    firestoreの動作確認

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,14 @@
     <h1>Nuxt.js(CompositionAPI)+TypeScriptでFirebaseと連携したい！</h1>
     <h2>↓googleでログイン↓</h2>
     <button @click="googleLogin">ログイン</button>
+    <hr />
+    <input type="text" v-model="memo.text" />
+    <button @click="addMemo">追加</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@nuxtjs/composition-api" //coompositionを使えるように
+import { reactive, defineComponent } from "@nuxtjs/composition-api" //coompositionを使えるように
 
 export default defineComponent({
   setup(_, { root }) {
@@ -23,7 +26,24 @@ export default defineComponent({
       }
     }
 
-    return { googleLogin }
+    /**
+     * メモ機能
+     */
+    let memo = reactive({
+      text: null
+    })
+    const addMemo = async () => {
+      try {
+        await root.$fire.firestore.collection("memos").add({
+          text: memo.text
+        })
+        memo.text = null
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    return { googleLogin, memo, addMemo }
   }
 })
 </script>
